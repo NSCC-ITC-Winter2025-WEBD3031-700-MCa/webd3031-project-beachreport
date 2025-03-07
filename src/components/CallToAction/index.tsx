f31a89d2c3f576fd1c9ed5456ec9fecb9f3db533
Andrea Morshead <andrea.morshead@gmail.com>
2025-03-07 12:22:04 -0400

    feat: added call to action section

--- src/components/CallToAction/index.tsx
+++ src/components/CallToAction/index.tsx
@@ -2,101 +2,44 @@ import Link from "next/link";
 
 const CallToAction = () => {
   return (
-    <section className="relative z-10 overflow-hidden bg-primary py-20 lg:py-[115px]">
-      <div className="container mx-auto">
-        <div className="relative overflow-hidden">
-          <div className="-mx-4 flex flex-wrap items-stretch">
-            <div className="w-full px-4">
-              <div className="mx-auto max-w-[570px] text-center">
-                <h2 className="mb-2.5 text-3xl font-bold text-white md:text-[38px] md:leading-[1.44]">
-                  <span>What Are You Looking For?</span>
-                  <span className="text-3xl font-normal md:text-[40px]">
-                    {" "}
-                    Get Started Now{" "}
-                  </span>
-                </h2>
-                <p className="mx-auto mb-6 max-w-[515px] text-base leading-[1.5] text-white">
-                  There are many variations of passages of Lorem Ipsum but the
-                  majority have suffered in some form.
-                </p>
-                <Link
-                  href="/"
-                  className="inline-block rounded-md border border-transparent bg-secondary px-7 py-3 text-base font-medium text-white transition hover:bg-[#0BB489]"
-                >
-                  Start using Play
-                </Link>
-              </div>
-            </div>
-          </div>
+    <div className="bg-white grid grid-cols-1 lg:grid-cols-12 py-10 items-center">
+     {/* Button Section */}
+      <div className="lg:col-span-3 lg:col-start-2 flex flex-col items-center text-2xl text-black">
+        <Link
+          href="/signup"
+          className="rounded-3xl border border-black bg-[#7AA38D] px-20 py-5 transition mb-3 hover:bg-[#9FD3B7]"
+        >
+          JOIN TODAY!
+        </Link>
+        <p className="mt-5 text-xl">To start rating Beaches!</p>
+        <div className="my-2">
+          <i className="fa-regular fa-star"></i>
+          <i className="fa-regular fa-star"></i>
+          <i className="fa-regular fa-star"></i>
+          <i className="fa-regular fa-star"></i>
+          <i className="fa-regular fa-star"></i>
         </div>
       </div>
-      <div>
-        <span className="absolute left-0 top-0">
-          <svg
-            width="495"
-            height="470"
-            viewBox="0 0 495 470"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx="55"
-              cy="442"
-              r="138"
-              stroke="white"
-              strokeOpacity="0.04"
-              strokeWidth="50"
-            />
-            <circle
-              cx="446"
-              r="39"
-              stroke="white"
-              strokeOpacity="0.04"
-              strokeWidth="20"
-            />
-            <path
-              d="M245.406 137.609L233.985 94.9852L276.609 106.406L245.406 137.609Z"
-              stroke="white"
-              strokeOpacity="0.08"
-              strokeWidth="12"
-            />
-          </svg>
-        </span>
-        <span className="absolute bottom-0 right-0">
-          <svg
-            width="493"
-            height="470"
-            viewBox="0 0 493 470"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx="462"
-              cy="5"
-              r="138"
-              stroke="white"
-              strokeOpacity="0.04"
-              strokeWidth="50"
-            />
-            <circle
-              cx="49"
-              cy="470"
-              r="39"
-              stroke="white"
-              strokeOpacity="0.04"
-              strokeWidth="20"
-            />
-            <path
-              d="M222.393 226.701L272.808 213.192L259.299 263.607L222.393 226.701Z"
-              stroke="white"
-              strokeOpacity="0.06"
-              strokeWidth="13"
-            />
-          </svg>
-        </span>
+
+
+      {/* Paragraph Section */}
+      <div className="lg:col-span-7 lg:col-start-6 lg:border-l border-black px-10 flex items-center">
+        <p className="font-akshar text-black text-2xl p-5 lg:ml-10">
+          Discovering the ideal beach on a warm summer day should be a breeze! With over
+          <strong> 7,500 km of coastline, </strong>
+          Nova Scotia ensures you&apos;re never more than 56 km from the ocean.
+          At Shorecast, we provide all the details you need to find the perfect beach for your adventure.
+          <br /><br />
+          Become a member to rate beaches and see what others are saying!
+          Your insights help fellow beachgoers discover the best spots,
+          and you&apos;ll gain access to real-time ratings on all beaches across the province.
+          <strong> Join today </strong>and make every beach day better!
+        </p>
       </div>
-    </section>
+    </div>
   );
 };
 
 export default CallToAction;
+
+
