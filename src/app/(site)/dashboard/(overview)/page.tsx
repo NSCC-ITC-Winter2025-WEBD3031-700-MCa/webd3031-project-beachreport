6209d42d58b7406ca34a3d3b9df24dc3710878c3
Kortney <kortney_foley@hotmail.com>
2025-04-01 11:32:01 -0300

    feat: complete admin dashboard

--- src/app/(site)/dashboard/(overview)/page.tsx
+++ src/app/(site)/dashboard/(overview)/page.tsx
@@ -1,17 +1,23 @@
 import { Suspense } from 'react';
 import { UsersChartSkeleton, LatestReportsSkeleton, CardsSkeleton, } from '@/app/ui/skeletons';
 import LatestReports from '@/app/ui/dashboard/latest-reports';
+import UsersChart from '@/app/ui/dashboard/users-chart';
+import CardWrapper from '@/app/ui/dashboard/cards';
  
 export default async function Page() {
     return (
       <div className="bg-gray-200 max-w-4xl mx-auto my-20 p-6">
+        <h1 className="mb-4 font-bold text-center text-xl md:text-2xl">
+          Admin Dashboard
+        </h1>
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
+        <CardWrapper />
         </Suspense>
         </div>
         <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<UsersChartSkeleton />}>
-          {/* <UsersChart /> */}
+          <UsersChart />
         </Suspense>
         <Suspense fallback={<LatestReportsSkeleton />}>
           <LatestReports />
--- src/app/ui/dashboard/latest-reports.tsx
+++ src/app/ui/dashboard/latest-reports.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
 
 export default function LatestReports() {
@@ -9,7 +10,7 @@ export default function LatestReports() {
     async function fetchReports() {
       const res = await fetch("/api/reports");
       const data = await res.json();
-      setReports(data.reports || []);
+      setReports(data.reports?.slice(0, 4) || []); // Limit to 6 most recent
       setLoading(false);
     }
     fetchReports();
@@ -21,39 +22,37 @@ export default function LatestReports() {
 
   return (
     <div className="flex w-full flex-col md:col-span-4">
-      <h2 className="mb-4 text-xl md:text-2xl font-semibold">Latest Beach Reports</h2>
+      <h2 className="mb-4 text-center text-xl md:text-2xl font-semibold">Latest Beach Reports</h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         <div className="bg-white px-6 py-4 rounded-lg shadow-md">
           {reports.map((report, i) => (
             <div
               key={report.id}
-              className={`flex flex-row items-center justify-between py-4 border-t ${i === 0 ? '' : 'border-gray-200'}`}
+              className={`flex flex-row items-start justify-between py-4 border-t ${
+                i === 0 ? "" : "border-gray-200"
+              }`}
             >
-              <div className="flex items-center">
-                
-                <div className="min-w-0">
-                  <p className="truncate text-sm font-semibold md:text-base">
-                    {report.beach.name} 
-                  </p>
-                  <p className="text-sm text-gray-500">
-                    <strong>User:</strong> {report.user.name} 
-                  </p>
-                  <p className="text-sm font-medium text-gray-700 md:text-base">
+              <div className="min-w-0">
+                <p className="truncate text-sm font-semibold md:text-base">{report.beach.name}</p>
+                <p className="text-sm text-gray-500">
+                  <strong>User:</strong> {report.user.name}
+                </p>
+                <p className="text-sm font-medium text-gray-700 md:text-base">
                   <strong>Rating:</strong> {report.rating} / 5
                 </p>
-                </div>
               </div>
-              <div className="min-w-0">
-                
-                <p className="text-sm text-gray-600">
-                  <strong>Report:</strong> {report.reportText}
-                </p>
+
+              <div className="h-full w-px bg-gray-300 mx-4"></div>
+
+              <div className="min-w-0 flex-1">
+                <p className="text-sm font-semibold text-black-600">Report:</p>
+                <p className="text-sm text-gray-600">{report.reportText}</p>
               </div>
             </div>
           ))}
         </div>
-        <div className="flex items-center pb-2 pt-6">
-          
+        <div className="flex justify-center items-center pb-2 pt-6">
+          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500">Updated just now</h3>
         </div>
       </div>
